5f4e1002382294ab68f96c596f8ad264066f1c64
Josh Humphries <j.humphries@nhm.ac.uk>
2018-10-25 22:58:55 +0100

    Expose the stats as an action instead of as a controller
    This allows more standardised authentication checking which is now required
    for the stats.

--- ckanext/ckanpackager/plugin.py
+++ ckanext/ckanpackager/plugin.py
@@ -1,6 +1,7 @@
 import ckan.plugins as p
 from ckanext.ckanpackager.lib.helpers import should_show_format_options
 from ckanext.ckanpackager.lib.utils import url_for_package_resource
+from ckanext.ckanpackager.logic import action, auth
 
 config = {}
 
@@ -10,6 +11,8 @@ class CkanPackagerPlugin(p.SingletonPlugin):
     p.implements(p.interfaces.IRoutes, inherit=True)
     p.implements(p.IConfigurable)
     p.implements(p.IConfigurer)
+    p.implements(p.IActions)
+    p.implements(p.IAuthFunctions)
 
     def configure(self, app_cfg):
         """Implementation of IConfigurable.configure
@@ -39,10 +42,6 @@ class CkanPackagerPlugin(p.SingletonPlugin):
         map_route.connect('package_resource', '/dataset/{package_id}/resource/{resource_id}/package',
                           controller='ckanext.ckanpackager.controllers.packager:CkanPackagerController',
                           action='package_resource')
-
-        map_route.connect('packager_stats', '/packager_stats', action='get_stats',
-                          controller='ckanext.ckanpackager.controllers.stats:CkanStatsController')
-
         return map_route
 
     def update_config(self, app_config):
@@ -53,3 +52,15 @@ class CkanPackagerPlugin(p.SingletonPlugin):
         p.toolkit.add_template_directory(app_config, 'theme/templates')
         p.toolkit.add_public_directory(config, 'theme/public')
         p.toolkit.add_resource('theme/public', 'ckanext-ckanpackager')
+
+    # IActions
+    def get_actions(self):
+        return {
+            u'packager_stats': action.get_packager_stats_action(config),
+        }
+
+    # IAuthFunctions
+    def get_auth_functions(self):
+        return {
+            u'packager_stats': auth.packager_stats,
+        }
